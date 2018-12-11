007ec183b980f5c16a0d267a075d47fd21ec52b8
SubMishMar <subodheenitr@gmail.com>
2018-12-11 02:59:20 -0600

    WIP

--- scripts/ground_truth_publisher.py
+++ scripts/ground_truth_publisher.py
@@ -4,8 +4,27 @@ import roslib
 import rospy
 import math
 import tf
+import tf2_ros
 import numpy as np
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import TransformStamped
+
+def publishInitTransform(time, trans_init, rot_init):
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+    static_transformStamped = TransformStamped()
+    static_transformStamped.header.stamp = time
+    static_transformStamped.header.frame_id = "world"
+    static_transformStamped.child_frame_id = "init_frame"
+  
+    static_transformStamped.transform.translation.x = trans_init[0]
+    static_transformStamped.transform.translation.y = trans_init[1]
+    static_transformStamped.transform.translation.z = trans_init[2]
+  
+    static_transformStamped.transform.rotation.x = rot_init[0]
+    static_transformStamped.transform.rotation.y = rot_init[1]
+    static_transformStamped.transform.rotation.z = rot_init[2]
+    static_transformStamped.transform.rotation.w = rot_init[3]
+    broadcaster.sendTransform(static_transformStamped)
 
 if __name__ == '__main__':
 
@@ -19,6 +38,7 @@ if __name__ == '__main__':
 
     rate = rospy.Rate(10.0)
 
+    firstTime = True
     while not rospy.is_shutdown():
         
         time = rospy.get_rostime()
@@ -27,7 +47,13 @@ if __name__ == '__main__':
             (trans,rot) = listener.lookupTransform('/world', '/base_link', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-        print([trans, rot])
+        if firstTime:
+        	trans_init = trans
+        	rot_init = rot
+        	firstTime = False
+        
+        publishInitTransform(time, trans_init, rot_init)
+
         pose.header.stamp = time
         pose.header.frame_id = "world"
         pose.pose.position.x = trans[0]
@@ -38,4 +64,5 @@ if __name__ == '__main__':
         pose.pose.orientation.z = rot[2]
         pose.pose.orientation.w = rot[3]
         pose_pub.publish(pose)
+
         rate.sleep()
